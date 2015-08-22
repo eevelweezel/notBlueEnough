8d6fcd858215079852774986b11c9ee2882ae3a4
Heather White <eevel.weezel@gmail.com>
2015-08-21 19:47:18 -0500

    Added support for gradients and transparency.

--- notBlueEnough.py
+++ notBlueEnough.py
@@ -1,29 +1,29 @@
 import sys;
 import os;
 from PIL import Image;
-import itertools;
+
 
 """
-Batch-process all images in a directory, replacing all pixels of the of the offending color.
-Because brands and stuff.
+Batch-update images in a directory, replacing all pixels of the of the offending color and an adjustable gradient.
+Because marketing.  It does preserve transperancy, but doesn't do so well with .GIFs.  Yet.
 Writes output to a subdirectory named "out."
+
 dir = input directory
 new = hex value (w/o the #) of the replacement color
-splat args = space-delimited list of hex values to replace (w/o the #s)
+rep = hex value of the color to be replaced (w/o the #s)
+fuzz = size of the gradient.
 
 """
 
-
-def main(null, dir, new, *args):
+def main(null, dir, new, rep, fuzz):
     rootDir = dir
-    print(args)
+    n = int(fuzz)
     if os.path.exists(rootDir+'/out'): 
         pass
     else:
         os.mkdir(rootDir+'/out')
     new = bluify(new)
-    old = [bluify(x) for x in args]
-    print(old)
+    old = bluify(rep)
     for dirName, subdirList, fileList in os.walk(rootDir):
         for fname in fileList:
             im = Image.open(rootDir+'/'+fname) 
@@ -36,11 +36,12 @@ def main(null, dir, new, *args):
                             if len(pixel[x,y]) == 3:
                                 r,g,b = pixel[x,y]
                             else: 
-                                r,g,b,a = pixel[x,y]
-                            if (r, g, b) in old:
-                                pixel[x, y] = new        
+                                r,g,b,a = pixel[x,y]  
+                            if r in range(old[0]-n,old[0]+n) and g in range(old[1]-n,old[1]+n) and b in range(old[2]-n,old[2]+n):
+                                offset = (r-old[0],g-old[1],b-old[2])
+                                pixel[x,y] = (new[0]-offset[0],new[1]-offset[1],new[2]-offset[2],a)
                         except TypeError:
-                            print('Some kinda type weirdness: '+fname)          
+                            print('Some kinda type weirdness: '+fname)                              
             im.save(rootDir+'/out/'+fname) 
     return
     
@@ -50,7 +51,6 @@ def bluify(thing):
     b = int(thing[2:4], 16)
     c = int(thing[4:6], 16)
     return (a, b, c)
-    
     
     
 if __name__ == '__main__':
