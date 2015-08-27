066f5419845304d92779e4fbabafb8c53a715ae1
Heather White <eevel.weezel@gmail.com>
2015-08-26 19:35:09 -0500

    Update notBlueEnough.py

--- notBlueEnough.py
+++ notBlueEnough.py
@@ -1,6 +1,7 @@
 import sys;
 import os;
 from PIL import Image;
+
 """
 Batch-update images in a directory, replacing all pixels of the of the offending color and an adjustable gradient.
 Because marketing.  It preserves transperancy and does ...ok... withh GIFs.
@@ -12,6 +13,7 @@ rep = hex value of the color to be replaced (w/o the #s)
 fuzz = size of the gradient.
 
 """
+
 def main(null, dir, new, rep, fuzz):
     rootDir = dir
     n = int(fuzz)
@@ -27,42 +29,24 @@ def main(null, dir, new, rep, fuzz):
                 im = Image.open(rootDir+'/'+fname) 
             except IOError:
                 print(fname + ' has issues.')   
-            if im.format == 'GIF':
-                im = im.convert('RGBA')
-                pixel = im.load()
-                width, height = im.size
-                for x in range(width):
-                    for y in range(height):
-                        if type(pixel[x,y]) != int:  
-                            try:                    
-                                if len(pixel[x,y]) == 3:
-                                    r,g,b = pixel[x,y]
-                                else: 
-                                    r,g,b,a = pixel[x,y]
-                                off = offset(old,n)         
-                                if r in range(off[0][0],off[0][1]) and g in range(off[1][0],off[1][1]) and b in range(off[2][0],off[2][1]):
-                                    diff = (r-old[0],g-old[1],b-old[2])
-                                    pixel[x,y] = (new[0]-diff[0],new[1]-diff[1],new[2]-diff[2],a)
-                            except TypeError:
-                                print('Some kinda type weirdness: '+fname)        
-            else:
-                pixel = im.load()
-                width, height = im.size
-                for x in range(width):
-                    for y in range(height):
-                        if type(pixel[x,y]) != int:  
-                            try:                    
-                                if len(pixel[x,y]) == 3:
-                                    r,g,b = pixel[x,y]
-                                else: 
-                                    r,g,b,a = pixel[x,y]
-                                off = offset(old,n)         
-                                if r in range(off[0][0],off[0][1]) and g in range(off[1][0],off[1][1]) and b in range(off[2][0],off[2][1]):
-                                    diff = (r-old[0],g-old[1],b-old[2])
-                                    pixel[x,y] = (new[0]-diff[0],new[1]-diff[1],new[2]-diff[2],a)
-                            except TypeError:
-                                print('Some kinda type weirdness: '+fname)
-                            
+            im = im.convert('RGBA')
+            pixel = im.load()
+            width, height = im.size
+            for x in range(width):
+                for y in range(height):
+                    if type(pixel[x,y]) != int:  
+                        try:                    
+                            if len(pixel[x,y]) == 3:
+                                r,g,b = pixel[x,y]
+                            else: 
+                                r,g,b,a = pixel[x,y]
+                            off = offset(old,n)         
+                            if r in range(off[0][0],off[0][1]) and g in range(off[1][0],off[1][1]) and b in range(off[2][0],off[2][1]):
+                                diff = (r-old[0],g-old[1],b-old[2])
+                                pixel[x,y] = (new[0]-diff[0],new[1]-diff[1],new[2]-diff[2],a)
+                        except TypeError:
+                            print('Some kinda type weirdness: '+fname)        
+                           
             im.save(rootDir+'/out/'+fname) 
     return
     
@@ -72,7 +56,8 @@ def bluify(thing):
     b = int(thing[2:4], 16)
     c = int(thing[4:6], 16)
     return (a, b, c)
-    
+
+#TODO: check the math here...       
 def offset(nold, fuzz):
     out = []
     for i in nold:
